a23d2d23f9f968394add073aab97985d37d1b04a
Arul-Samuel-A <arulsamuela@gmail.com>
2024-10-29 17:39:49 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuIcon = document.getElementById('menu-icon');
-const navMenu = document.getElementById('nav-menu');
-
-
-menuIcon.addEventListener('click', () => {
-    navMenu.style.display = navMenu.style.display === 'block' ? 'none' : 'block';
-});
-
